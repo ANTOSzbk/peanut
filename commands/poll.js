4f1af94065ec204ce7e3ec601eb93eed7c5683ed
ANTOSzbk <antoos123@gmail.com>
2020-01-06 03:32:01 +0100

    added command 'poll' and 'yn'

---
+++ commands/poll.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+const _cooldown = new Discord.Collection();
+
+exports.run = (client, message, args) => {
+    const now = Date.now();
+    const expirationDate = _cooldown.get(message.author.id) + parseInt(this.cooldown);
+    const timeleft = (expirationDate - now) / 1000;
+    if (_cooldown.has(message.author.id))
+        return message.reply(`Możesz tworzyć ankietę raz na **2** minuty. Odczekaj ${timeleft.toFixed(0)} sekund przed ponownym użyciem.`)
+    if (!args.length)
+        return message.reply(`🗳️ Brak danych do utworzenia ankiety. Użyj: \n \`!poll [pytanie]\` - odpowiedzi tak/nie \n`);
+    //\`!poll '[pytanie]' '[odp1]' '[odp2]' itd..\` - maksymalnie 4 odpowiedzi`);
+    if (args.length > 4 && args.includes(`'`)) {
+        return message.reply(`🗳️ Za dużo odpowiedzi, max 4.`)
+            .then(message => message.delete(3000))
+            .catch(console.error);
+    }
+    _question = args.join(' ');
+    const pollEmbed = new Discord.RichEmbed()
+        .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
+        .setTitle(`🗳️ ${_question}`)
+        .setColor('GREEN')
+        .setFooter(`Pytanie zadane przez ${message.guild.member(message.author).displayName}.`)
+        .addField('\u200b', '✔️ Tak', true)
+        .addField('\u200b', '❌ Nie', true)
+        .setTimestamp()
+    message.channel.send(pollEmbed).then(async message => {
+        await message.react('✔️');
+        await message.react('❌');
+    })
+    if (!_cooldown.has(message.author.id)) _cooldown.set(message.author.id, now)
+    setTimeout(() => {
+        _cooldown.delete(message.author.id);
+    }, this.cooldown);
+}
+
+module.exports.aliases = ['ankieta', 'sonda', 'glosowanie', 'głosowanie']
+module.exports.cooldown = '120000';
---
+++ commands/yn.js
@@ -0,0 +1,12 @@
+exports.run = (client, message, args) => {
+    const responseArr = ['Tak', 'Nie', 'Mój wywiad donosi: NIE', 'Prawdopodobnie', 'Prawie jak tak', 'YES, YES, YES', 'Tego nawet ja nie wiem']
+    const randomResponse = responseArr[Math.floor(Math.random() * responseArr.length)]
+    if(!args.length) return message.reply('Nie wiem, zadaj najpierw pytanie');
+
+    message.reply(randomResponse);
+    if(args.includes('?')) args[args.length].slice(args[args.length-1]);
+    if(args[0].toLowerCase() === 'czy' && args[2].toLowerCase() === 'to') {
+        if (randomResponse === 'Tak' || randomResponse === 'YES, YES, YES') return message.reply(`${args[1]} to ${args.slice(3, args.length).join(' ')}`);
+        if (randomResponse === 'Nie' || randomResponse === 'Mój wywiad donosi: NIE') return message.reply(`${args[1]} to nie ${args.slice(3, args.length).join(' ')}`);
+    }
+}
